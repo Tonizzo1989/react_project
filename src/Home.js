782d474876036ec22314467b387933aef44283b1
Tonizzo1989 GitHub <toninorizzo1989@yahoo.it>
2022-09-04 11:40:05 +0200

    Componenti

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,17 @@
-const Home = () => {
+import { useState } from "react";
+import BlogList from "./BlogList";
 
-    const handleClick = () => {
-        console.log('Bravissimo');
-    }
+
+const Home = () => {
+    const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'pippo', id: 3}
+]);
     
     return (  
         <div className="home">
-            <h2>Homepage</h2>
-            <button onClick={handleClick()}>Click me!</button>
-            
+        <BlogList blogs = {blogs} title = "All Blogs!"/>
         </div>
     );
 }
