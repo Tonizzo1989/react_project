fa615d8c6db897a1ebfefdd70e80a10e8c7b614e
Tonizzo1989 GitHub <toninorizzo1989@yahoo.it>
2022-09-11 12:35:49 +0200

    aggiunti pulsanti e css al blog

--- src/Create.js
+++ src/Create.js
@@ -1,8 +1,45 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('');
+    
+
     return (  
     <div className="create">
      
      <h2> Add a new blog </h2>
+     <form>
+        <label> Titolo : </label>
+        <input
+        type= "text" 
+        required
+        value = { title }
+        onChange = { (e) => setTitle (e.target.value)}
+        />
+        <label> Sottotitolo </label>
+        <textarea
+        required
+        value={body}
+        onChange = {(e) => setBody (e.target.value)}
+        ></textarea>
+        <label> Autore: </label>
+        <select 
+        value={author}
+        onChange = {(e) => setAuthor (e.target.value)}
+        >
+            <option value="mario"> Mario </option>
+            <option value="yoshi"> Yoshi </option>
+        </select>
+        <button> Aggiungi Blog </button>
+        <p> { title } </p>
+        <p> { body } </p>
+        <p> { author } </p>
+
+
+     </form>
+     
 
     </div>
        
